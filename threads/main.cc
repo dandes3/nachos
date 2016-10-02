57cc3dbfefe8890b490628f3e8c29cef4602e0f3
Don Andes <drandes@email.wm.edu>
2016-10-02 01:09:06 -0400

    Modified main.cc to ease merge conflicts

--- threads/main.cc
+++ threads/main.cc
@@ -54,8 +54,10 @@
 #include "system.h"
 
 // External functions used by this file
-
+extern void Two(int numP, int numC, int buffSize);
+extern void Four(int numCars);
 extern void ThreadTest(void), Copy(char *unixFile, char *nachosFile);
+extern void ElevatorTest(void);
 extern void Print(char *file), PerformanceTest(void);
 extern void StartProcess(char *file), ConsoleTest(char *in, char *out);
 extern void MailTest(int networkID);
@@ -85,98 +87,99 @@ main(int argc, char **argv)
     
 #ifdef THREADS
     ThreadTest();
+    DEBUG('t', "ThreadTest ended\n");
 #endif
 
     for (argc--, argv++; argc > 0; argc -= argCount, argv += argCount) 
     {
-    	argCount = 1;
+	argCount = 1;
         if (!strcmp(*argv, "-z"))               // print copyright
-        {           
             printf (copyright);
+
+
+// Begin code section from main.cc modifed for argument checking purposes --
+#ifdef THREADS
+        if (!strcmp(*argv, "-p") || !strcmp(*argv, "-P"))
+        {
+            if (!strcmp(*(argv+1), "2"))                      // Run producer/consumer
+            {
+                int numberOfProducers = atoi(*(argv+2));
+                int numberOfConsumers = atoi(*(argv+3));
+                int buffSize = atoi(*(argv+4));
+                Two( numberOfProducers, numberOfConsumers, buffSize );
+            }
+            if (!strcmp(*(argv+1), "3"))                      // Run THall elevator
+            {
+            	ElevatorTest();
+            }
+            if (!strcmp(*(argv+1), "4"))                      // Run bridge simulator
+            {
+                int numCars = atoi(*(argv+2));
+                Four(numCars);
+            }
         }
+#endif
+// End above code block -----------------------------------------------------
+
 
 #ifdef USER_PROGRAM
-
-        if (!strcmp(*argv, "-x"))               // run a user program
-        {        	
-	        ASSERT(argc > 1);
+        if (!strcmp(*argv, "-x")) {        	// run a user program
+	    ASSERT(argc > 1);
             StartProcess(*(argv + 1));
             argCount = 2;
-        } 
-        else if (!strcmp(*argv, "-c"))          // test the console
-        {
-        	if (argc == 1)
-        	{
-        		ConsoleTest(NULL, NULL);
-	        }
-	    }
-	    else 
-	    {
-		    ASSERT(argc > 2);
+        } else if (!strcmp(*argv, "-c")) {      // test the console
+	    if (argc == 1)
+	        ConsoleTest(NULL, NULL);
+	    else {
+		ASSERT(argc > 2);
 	        ConsoleTest(*(argv + 1), *(argv + 2));
 	        argCount = 3;
 	    }
-	    
 	    interrupt->Halt();		// once we start the console, then 
-					            // Nachos will loop forever waiting 
-					            // for console input
+					// Nachos will loop forever waiting 
+					// for console input
 	}
 #endif // USER_PROGRAM
 #ifdef FILESYS
-	if (!strcmp(*argv, "-cp"))          // copy from UNIX to Nachos
-	{ 		
+	if (!strcmp(*argv, "-cp")) { 		// copy from UNIX to Nachos
 	    ASSERT(argc > 2);
 	    Copy(*(argv + 1), *(argv + 2));
 	    argCount = 3;
-	} 
-	else if (!strcmp(*argv, "-p"))      // print a Nachos file
-	{	
+	} else if (!strcmp(*argv, "-p")) {	// print a Nachos file
 	    ASSERT(argc > 1);
 	    Print(*(argv + 1));
 	    argCount = 2;
-	} 
-	else if (!strcmp(*argv, "-r"))      // remove Nachos file
-	{	
+	} else if (!strcmp(*argv, "-r")) {	// remove Nachos file
 	    ASSERT(argc > 1);
 	    fileSystem->Remove(*(argv + 1));
 	    argCount = 2;
-	} 
-	else if (!strcmp(*argv, "-l"))      // list Nachos directory
-	{
-		fileSystem->List();
-	} 
-	else if (!strcmp(*argv, "-D"))      // print entire filesystem
-	{
-		fileSystem->Print();
-	} 
-	else if (!strcmp(*argv, "-t"))      // performance test
-	{
-		PerformanceTest();
+	} else if (!strcmp(*argv, "-l")) {	// list Nachos directory
+            fileSystem->List();
+	} else if (!strcmp(*argv, "-D")) {	// print entire filesystem
+            fileSystem->Print();
+	} else if (!strcmp(*argv, "-t")) {	// performance test
+            PerformanceTest();
 	}
-
 #endif // FILESYS
 #ifdef NETWORK
-        if (!strcmp(*argv, "-o")) 
-        {
-        	ASSERT(argc > 1);
+        if (!strcmp(*argv, "-o")) {
+	    ASSERT(argc > 1);
             Delay(2); 				// delay for 2 seconds
 						            // to give the user time to 
 						            // start up another nachos
-           
             MailTest(atoi(*(argv + 1)));
             argCount = 2;
         }
-
 #endif // NETWORK
-    }  //TODO: find where this ties to. 
+    }
 
     currentThread->Finish();	// NOTE: if the procedure "main" 
-				                // returns, then the program "nachos"
-				                // will exit (as any other normal program
-				                // would).  But there may be other
-				                // threads on the ready list.  We switch
-				                // to those threads by saying that the
-				                // "main" thread is finished, preventing
-				                // it from returning.
+				// returns, then the program "nachos"
+				// will exit (as any other normal program
+				// would).  But there may be other
+				// threads on the ready list.  We switch
+				// to those threads by saying that the
+				// "main" thread is finished, preventing
+				// it from returning.
     return(0);			// Not reached...
 }
