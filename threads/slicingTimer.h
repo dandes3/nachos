aec06f87c58279a16c8682d47250ea87fa02297a
Philip J  Favaloro <pjfavalo@calvin.cs.wm.edu>
2016-11-02 22:46:50 -0400

    Same as before, plus slicing timer files

---
+++ threads/slicingTimer.h
@@ -0,0 +1,26 @@
+#ifndef SLTIMER_H
+#define SLTIMER_H
+
+#include "copyright.h"
+#include "utility.h"
+
+// The following class defines a hardware timer. 
+class SlicingTimer {
+  public:
+    SlicingTimer(VoidFunctionPtr timerHandler, int callArg);
+				// Initialize the timer, to call the interrupt
+				// handler "timerHandler" every time slice.
+    ~SlicingTimer() {}
+
+// Internal routines to the timer emulation -- DO NOT call these
+
+    void TimerExpired();	// called internally when the hardware
+				// timer generates an interrupt
+
+  private:
+    VoidFunctionPtr handler;	// timer interrupt handler 
+    int arg;			// argument to pass to interrupt handler
+
+};
+
+#endif // TIMER_H
