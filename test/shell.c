641450393a1bcbae9bb18ff5b6a5ba203d82ed0b
Philip J  Favaloro <pjfavalo@dingo.cs.wm.edu>
2016-11-04 00:20:16 -0400

    Shell.c cleaned up

--- test/shell.c
+++ test/shell.c
@@ -28,7 +28,6 @@ main()
     buffer[--i] = '\0';
 
     if( i > 0 ) {
-    /*newProc = Fork();*/
         if ((newProc = Fork()) == 0) { /* Child */
            
             char curChar;
@@ -37,7 +36,6 @@ main()
             int argc = 0;
             int beforeFirstArg = -1; /* index of the space before the first argument */
             int firstSpace = 0; /* index of first space in buffer */
-            
             
             while ((curChar = buffer[count]) != '\0'){
                 if (buffer[count] == ' ' && firstSpace == 0) /* Sets the position of the first space in the buffer */
@@ -51,17 +49,8 @@ main()
                 
                 count ++;
             }
-            
-            prints("Argc:", ConsoleOutput);
-            printd(argc, ConsoleOutput);
-            prints("\n", ConsoleOutput);
-            
-            prints("beforeFirstArg is ", ConsoleOutput);
-            printd(beforeFirstArg, ConsoleOutput);
-            prints("\n", ConsoleOutput);
-            
+          
             if (argc == 0){ /* No arguments */
-                prints("No arguments\n");
                 Exec(buffer, (char**) 0);
             
                 prints("After exec\n", ConsoleOutput);
@@ -74,47 +63,28 @@ main()
                 int start = beforeFirstArg;
                 int end = beforeFirstArg + 1;
                 int argNum = 0;
-                int first = 0;
-                char temp[2];
                 int pos = 0;
                 int startPos = 0;
-                prints("yes arguments\n", ConsoleOutput);
-                while ((curChar = buffer[end]) != '\0'){
-                /*    prints("end is ", ConsoleOutput);
-                    printd(end, ConsoleOutput);
-                    prints("\n", ConsoleOutput);
-                  */  
-                    /*prints("curChar is ", ConsoleOutput);
-                    temp[0] = curChar;
-                    temp[1] = '\0';
-                    prints(temp, ConsoleOutput);
-                    prints("\n", ConsoleOutput);
-                   */ 
+       
+                while ((curChar = buffer[end]) != '\0'){ 
                     if (curChar == ' '){
-                     /*   prints("curChar == ' '\n", ConsoleOutput);*/
-                        prevChar = buffer[end-1];
-                        if (prevChar == ' '){  
-                            /*prints("prevChar == ' '\n", ConsoleOutput);*/
+                   
+                        if (buffer[end - 1] == ' '){  
                             end ++;
                             start ++;
                         }
                      
                         else{ 
                          /* start is the space before arg, end is the space after arg */
-			/*    prints("prevChar != ' '\n", ConsoleOutput);*/
                             startPos = 0;
                             for (start = ++start; start < end; start++){
-                                Write(&buffer[start], 1, ConsoleOutput);
                                 curArg[pos++] = buffer[start];
                                 startPos ++;
                             }
                  
                             curArg[++pos] = '\0';
                             args[argNum] = curArg + (pos - startPos - 1);
-                            prints("arg is ", ConsoleOutput);
-                            prints(args[argNum], ConsoleOutput);
-                            prints("\n", ConsoleOutput);
-                            
+                                
                             argNum ++;
                             end ++;
                             pos ++;
@@ -125,34 +95,15 @@ main()
                         end ++;
                     
                 }
+
                 startPos = 0;
                 for (start = ++start; start < end; start++){
                     curArg[pos++] = buffer[start];
                     startPos ++;
-                 }
-                
+                 }                
                 curArg[++pos] = '\0';      
                 args[argNum] =  curArg + (pos - startPos - 1);
 
-                prints("arg is ", ConsoleOutput);
-                prints(args[argNum], ConsoleOutput);
-                prints("\n", ConsoleOutput);
-               
-               printd(argNum, ConsoleOutput);
-               prints("\n", ConsoleOutput); 
-               args[argNum + 1] = (char *) 0 ;
-
-            /*   
-                for (count = 0; args[count] != (char *) 0; count ++){
-	           prints("In aggs print for\n");
-                   prints(args[count], ConsoleOutput);
-                }
-                if (args[0] == (char *) 0)
-			prints("Bite me\n", ConsoleOutput);
-                printd((int) args[0], ConsoleOutput);
-                prints("\n", ConsoleOutput);
-                prints("\nPast args print", ConsoleOutput);
-*/
                 buffer[firstSpace] = '\0';
                 Exec(buffer, (char**) args);
             } 
