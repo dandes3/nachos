43e6878dbd4eb8769affed8ddda99540298b4696
Philip J  Favaloro <pjfavalo@dingo.cs.wm.edu>
2016-11-03 21:47:17 -0400

    Messed around with shell

--- test/shell.c
+++ test/shell.c
@@ -78,38 +78,40 @@ main()
                 int pos = 0;
                 prints("yes arguments\n", ConsoleOutput);
                 while ((curChar = buffer[end]) != '\0'){
-                    prints("end is ", ConsoleOutput);
+                /*    prints("end is ", ConsoleOutput);
                     printd(end, ConsoleOutput);
                     prints("\n", ConsoleOutput);
-                    
-                    prints("curChar is ", ConsoleOutput);
+                  */  
+                    /*prints("curChar is ", ConsoleOutput);
                     temp[0] = curChar;
                     temp[1] = '\0';
                     prints(temp, ConsoleOutput);
                     prints("\n", ConsoleOutput);
-                    
+                   */ 
                     if (curChar == ' '){
-                        prints("curChar == ' '\n", ConsoleOutput);
+                     /*   prints("curChar == ' '\n", ConsoleOutput);*/
                         prevChar = buffer[end-1];
-                        if (prevChar == ' '){ /* iterate past space */
-                            prints("prevChar == ' '\n", ConsoleOutput);
+                        if (prevChar == ' '){  
+                            /*prints("prevChar == ' '\n", ConsoleOutput);*/
                             end ++;
                             start ++;
                         }
-                        
-                        else{ /* start is the space before arg, end is the space after arg */
-                            prints("prevChar != ' '\n", ConsoleOutput);
+                     
+                        else{ 
+                         /* start is the space before arg, end is the space after arg */
+			/*    prints("prevChar != ' '\n", ConsoleOutput);*/
                             pos = 0;
                             for (start = ++start; start < end; start++)
                                 args[argNum][pos++] = buffer[start];
-
-                            args[argNum][++start] = '\0';
+                                   
+                            args[argNum][++pos] = '\0';
                             prints("arg is ", ConsoleOutput);
                             prints(args[argNum], ConsoleOutput);
                             prints("\n", ConsoleOutput);
                             
                             argNum ++;
                             end ++;
+                            start++;
                         }
                     }
                     
@@ -121,13 +123,19 @@ main()
                 for (start = start++; start < end; start++)
                     args[argNum][pos++] = buffer[start];
                             
-                args[argNum][++start] = '\0';
+                args[argNum][++pos] = '\0';
                 prints("arg is ", ConsoleOutput);
                 prints(args[argNum], ConsoleOutput);
                 prints("\n", ConsoleOutput);
-                
+               
+               printd(argNum, ConsoleOutput);
+               prints("\n", ConsoleOutput); 
                 args[argNum + 1] = (char*) 0;
-                
+               
+                for (count = 0; args[count] != 0; count ++)
+                   prints(args[count], ConsoleOutput);
+                prints("Past args print", ConsoleOutput);
+
                 buffer[firstSpace] = '\0';
                 Exec(buffer, (char**) args);
             } 
